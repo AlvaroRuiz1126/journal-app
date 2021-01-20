5b91532ba13be6afa9eb2b6642421f8bbcc21c73
Alvaro Ruiz <cheruiz.1126@gmail.com>
2021-01-20 00:45:32 -0500

    add docs

---
+++ docs/precache-manifest.9d620536a826cd42f12321f5cba4b652.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92ebb372e079eac6c3995970cb797a48",
+    "url": "/index.html"
+  },
+  {
+    "revision": "17439e7231e385d22077",
+    "url": "/static/css/main.b6708bfd.chunk.css"
+  },
+  {
+    "revision": "4d25f51f9ecdf14f4004",
+    "url": "/static/js/2.15b6b938.chunk.js"
+  },
+  {
+    "revision": "94efaf9ef8dfdcc9b58d075c8086b235",
+    "url": "/static/js/2.15b6b938.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "17439e7231e385d22077",
+    "url": "/static/js/main.0f94b7ab.chunk.js"
+  },
+  {
+    "revision": "3d4175f9165103e13d56",
+    "url": "/static/js/runtime-main.b8be2bc2.js"
+  }
+]);
